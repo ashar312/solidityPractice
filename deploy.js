d321d3abafe4b9abcb88ab8f7db5099353bda8b5
ashar312 <ashar.ashfaq2@gmail.com>
2021-12-19 03:01:34 +0500

    Can simply deploy to Rinkeby network

--- deploy.js
+++ deploy.js
@@ -15,12 +15,12 @@ const deploy = async () => {
  
   console.log('Attempting to deploy from account', accounts[0]);
  
-  // const result = await new web3.eth.Contract(abi)
-  //   .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
-  //   .send({ gas: '1000000', from: accounts[0] });
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
  
-  // console.log('Contract deployed to', result.options.address);
-  // provider.engine.stop();
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
 };
  
 deploy();
