4c71d76a527aa3cbd1d397ba94c409b3ecc2fd99
ashar312 <ashar.ashfaq2@gmail.com>
2021-12-19 03:00:03 +0500

    working on Inbox

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+ 
+const { abi, evm } = require('./compile');
+ 
+provider = new HDWalletProvider(
+    'apart ancient regret soft farm dilemma size feed oppose list modify income',
+    'https://rinkeby.infura.io/v3/38ed1f2a2e9d4292acebe4e474abbf4d'
+);
+ 
+const web3 = new Web3(provider);
+ 
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+ 
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  // const result = await new web3.eth.Contract(abi)
+  //   .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+  //   .send({ gas: '1000000', from: accounts[0] });
+ 
+  // console.log('Contract deployed to', result.options.address);
+  // provider.engine.stop();
+};
+ 
+deploy();
